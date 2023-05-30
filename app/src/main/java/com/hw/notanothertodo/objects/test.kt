429599e4372d9af904db0ba1cb9609cd4c13405b
jason <jason.beutler@seattlecolleges.edu>
2023-05-29 19:51:40 -0700

    login and user signup pt 1

---
+++ app/src/main/java/com/hw/notanothertodo/objects/test.kt
@@ -0,0 +1,29 @@
+//package com.hw.notanothertodo.objects
+//
+//fun main(){
+//    test1()
+//}
+//
+//fun test1(){
+//    val pizza= User("jason", "itsjpb@pm.me")
+//    pizza.startUp()
+//    val cats = pizza.categories
+//    for (cat in cats){
+//        println(cat.key)
+//    }
+//    pizza.addCategory("warsaw")
+//
+//    val trouble = pizza.getNextTaskID()
+//    println(trouble)
+//    pizza.addTask(Task(trouble,"dishes", 7, false, 2 ))
+//    println(trouble)
+//    val task = pizza.currentTasks[0]
+//    println(task?.title)
+//    for (cat in cats){
+//        println(cat.key)
+//    }
+//    pizza.currentTasks[0]?.let { pizza.archiveTask(it) }
+//    println(pizza.getCurrentPoints())
+//
+//}
+
