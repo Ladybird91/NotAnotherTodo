657ac31da2286f99c6fa1c41083d82b38db2a0cc
jason <jason.beutler@seattlecolleges.edu>
2023-06-11 23:37:36 -0700

    early stuff

--- app/src/main/java/com/hw/notanothertodo/MainActivity.kt
+++ app/src/main/java/com/hw/notanothertodo/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.hw.notanothertodo.ui.theme.NotAnotherTodoTheme
 
+
+
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
